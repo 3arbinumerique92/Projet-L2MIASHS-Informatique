21925b380c6a4443ee3b6fb3d2cf28ecd684a527
100moins8.py <151393255+2sans13@users.noreply.github.com>
2023-11-21 13:26:29 +0100

    Commit 4

---
+++ Projet-V4.py
@@ -0,0 +1,80 @@
+ # Projet L2 MIASHS - Informatique: Mini Game     
+
+
+#        2-AFFICHAGE DE BASE
+
+#   2.1-Carte
+
+
+def display_map (m, d):
+    for i in range(len (m)) :
+        for j in range(len (m[i])):
+            if m[i][j] in d:
+                m[i][j]=d[m[i][j]]
+    return m
+                                 
+dico = {0:' ',1: '#'}
+map = [[0,0,0, 1,1], [0,0,0,0,1,],[1,1,0,0,0,], [0,0,0,0,0]]
+       
+#for i in range(len (map)):
+    #for j in range(len (map[i])):
+        #print(display_map (map, dico)[i][j], end=' ')
+    #print(" ")   
+       
+
+#   2.2-Personnage
+       
+#1)
+def create_perso (n,m) :
+    return {"char":"o", "x":n, "y":m}
+    
+p=create_perso (0,0)
+#print (d)
+
+#3)
+def display_map_and_char (m, d,p):
+    m=display_map (m, d)
+    m[p["x"]][p["y"]]=p["char"]
+    return m
+
+
+#4)
+for i in range(len(map)):
+    for j in range(len(map[i])):
+        print(display_map_and_char(map, dico, p)[i][j], end=' ')
+    print(" ")
+    
+
+#   2.3-Déplacement
+
+#1)
+
+def update_p(letter,p,m):              #modification 3.1
+    Haut_Bas={"z":-1, "s":1}
+    Gauche_Droite={"q":-1, "d":1}
+    
+    if letter in Haut_Bas:
+        p["x"]+=Haut_Bas[letter]
+        if p["x"]<0 or p["x"]>=len(m):
+            p["x"]-=Haut_Bas[letter]
+        if m[p["x"]][p["y"]]=="#":
+            p["x"]-=Haut_Bas[letter]
+    if letter in Gauche_Droite:
+        p["y"]+=Gauche_Droite[letter]
+        if p["y"]<0 or p["y"]>=len(m[0]):
+            p["y"]-=Gauche_Droite[letter]
+        if m[p["x"]][p["y"]]=="#":
+            p["y"]-=Gauche_Droite[letter]    
+    return p
+
+
+# 2.4 Jouer à l'infini
+while True:
+    letter=input("Quel déplacement? ”z” (haut), ”q” (gauche), ”s” (bas), et ”d” (droite):")  
+    p=update_p(letter,p,map)
+    map = [[0,0,0, 1,1], [0,0,0,0,1,],[1,1,0,0,0,], [0,0,0,0,0]]
+    map=display_map(map, dico)
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            print(display_map_and_char(map, dico, p)[i][j], end=' ')
+        print(" ")
